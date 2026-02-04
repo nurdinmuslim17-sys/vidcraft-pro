83c7aa8a82a173a34b3357f044c26329139711e6
nurdinmuslim17-sys <nurdinmuslim17@gmail.com>
2026-02-04 23:42:30 +0700

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,13 +1,11 @@
-export default async function handler(req, res) {
+export default async function handler(req,res){
   const { url } = req.query;
-  if (!url) {
-    return res.status(400).json({ error: "No TikTok URL" });
-  }
-  try {
+  if(!url) return res.status(400).json({error:"No TikTok URL"});
+  try{
     const r = await fetch(`https://tikwm.com/api/?url=${encodeURIComponent(url)}`);
     const data = await r.json();
     res.status(200).json(data);
-  } catch(err) {
-    res.status(500).json({ error: "Fetch failed" });
+  }catch(err){
+    res.status(500).json({error:"Fetch failed"});
   }
 }
